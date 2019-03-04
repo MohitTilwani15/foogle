f2fbfbdeb785dfbc0c5b8b13b850badfa3834f97
Mohit Tilwani <mtilwani@deloitte.de>
2019-03-04 15:11:51 +0100

    add console

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -42,6 +42,7 @@ export default {
 
   methods: {
     login() {
+      console.log(keys);
       window.location.href = `https://foursquare.com/oauth2/authenticate?client_id=${keys.default.clientID}&response_type=code&redirect_uri=${keys.default.redirectUri}`;
     },
 
