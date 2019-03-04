b464aa1d39b897265f8411f22d8a5213e0b3030b
Mohit Tilwani <mtilwani@deloitte.de>
2019-03-04 15:31:16 +0100

    add console

--- src/config/prod.js
+++ src/config/prod.js
@@ -1,5 +1,5 @@
 export default {
   clientID: 'AUBDYWZ0XJE3IWHHJGPZHKX2XVENLEPBWTRMPW40TJ0XG5DW',
   clientSecret: 'G2SF1Q4TLXHMVI1BLVY514LIQARD1KV3AODWIWJ4X2JD1O4K',
-  redirectUri: 'http://localhost:8080/auth/callback',
+  redirectUri: 'https://foogleapp.herokuapp.com/auth/callback',
 };
